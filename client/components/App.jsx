7c7c4fb50c490cdea3299f9d032a6f4cba45017f
Molly Fuhrman <mollyfuhrman@gmail.com>
2021-01-18 14:01:25 -0800

    Fixed styles.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
-const axios = require('axios');
-
 import Description from './Description.jsx';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
+
+const axios = require('axios');
 
 class App extends React.Component {
   constructor() {
@@ -13,8 +12,8 @@ class App extends React.Component {
       id: 1,
       descriptionData: [],
       itemTableData: [],
-      loaded: false
-    }
+      loaded: false,
+    };
     this.renderView = this.renderView.bind(this);
   }
 
@@ -24,28 +23,27 @@ class App extends React.Component {
         this.setState({
           descriptionData: data[0],
           itemTableData: data[1],
-          loaded: true
-        })
-      })
+          loaded: true,
+        });
+      });
   }
 
   renderView() {
     if (!this.state.loaded) {
-      return <h1>Loading...</h1>
+      return <h1>Loading...</h1>;
     }
-      return (
-        <div className="descriptionTab">
-          <Header />
-          <Description
+    return (
+      <div className="descriptionTab">
+        <Header />
+        <Description
           description={ this.state.descriptionData }
           itemDataTable={ this.state.itemTableData }
-          />
-          <div className="footers">
-            <Footer />
-          </div>
+        />
+        <div className="footers">
+          <Footer />
         </div>
-      )
-    }
+      </div>
+    );
   }
 
   render() {
@@ -53,7 +51,7 @@ class App extends React.Component {
       <main>
         { this.renderView() }
       </main>
-    )
+    );
   }
-
+}
 export default App;
