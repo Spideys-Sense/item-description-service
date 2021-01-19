8e48ceb06cfc31b9c609826a5d84aec0f0fd2f02
Molly Fuhrman <mollyfuhrman@gmail.com>
2021-01-19 11:44:28 -0800

    added tabClicked function, added class changes based on current tab

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,9 +4,6 @@ import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import SideBar from './SideBar.jsx';
 
-{/* <ItemDataTable itemDataTable={ itemDataTable } videoUrl={ description[0].videoUrl } /> */}
-
-
 const axios = require('axios');
 
 class App extends React.Component {
@@ -17,8 +14,12 @@ class App extends React.Component {
       descriptionData: [],
       itemTableData: [],
       loaded: false,
+      descriptionIsClicked: true,
+      nutritionalInfoTabClicked: false,
+      feedingInstructionsClicked: false,
     };
     this.renderView = this.renderView.bind(this);
+    this.tabClicked = this.tabClicked.bind(this);
   }
 
   componentDidMount() {
@@ -32,28 +33,108 @@ class App extends React.Component {
       });
   }
 
-  // tabClicked(e) {
-
-  // }
+  tabClicked(e) {
+    e.preventDefault();
+    console.log('clicked!')
+    console.log(e.target.className)
+    if (e.target.className === 'DescriptionTabText') {
+      this.setState({
+        descriptionIsClicked: true,
+        nutritionalInfoTabClicked: false,
+        feedingInstructionsClicked: false,
+      })
+    } else if (e.target.className === 'NutritionalInfoTabText') {
+      this.setState({
+        descriptionIsClicked: false,
+        nutritionalInfoTabClicked: true,
+        feedingInstructionsClicked: false,
+      })
+    } else if (e.target.className === 'FeedingInstructionsTabText') {
+      this.setState({
+        descriptionIsClicked: false,
+        nutritionalInfoTabClicked: false,
+        feedingInstructionsClicked: true,
+      })
+    }
+  }
 
   renderView() {
     if (!this.state.loaded) {
       return <h1>Loading...</h1>;
     }
-    return (
-      <div className="descriptionTab">
-        <Header />
-        <Description
-          description={ this.state.descriptionData }
-        />
-        <SideBar
-          itemDataTable={ this.state.sideBarData }
-          videoUrl={ this.state.descriptionData[0].videoUrl }
-          brand={ this.state.sideBarData[0].brand }
-        />
-        <Footer brand={ this.state.sideBarData[0].brand } />
-      </div>
-    );
+    if (this.state.descriptionIsClicked) {
+      return (
+        <div>
+          <div className="descriptionTabClickedTrue">
+            <Header tabClicked={ this.tabClicked }/>
+            <Description
+              description={ this.state.descriptionData }
+            />
+            <SideBar
+              itemDataTable={ this.state.sideBarData }
+              videoUrl={ this.state.descriptionData[0].videoUrl }
+              brand={ this.state.sideBarData[0].brand }
+            />
+            <Footer brand={ this.state.sideBarData[0].brand } />
+          </div>
+          <div className="nutritionalInfoTabClickedFalse"></div>
+          <div className="feedingInstructionsClickedFalse"></div>
+        </div>
+      );
+    }
+    if (this.state.nutritionalInfoTabClicked) {
+      return (
+        <div>
+          <div className="descriptionTabClickedFalse"></div>
+          <div className="nutritionalInfoTabClickedTrue">
+            <Header tabClicked={ this.tabClicked } />
+            <Description
+              description={ this.state.descriptionData }
+            />
+            <SideBar
+              itemDataTable={ this.state.sideBarData }
+              videoUrl={ this.state.descriptionData[0].videoUrl }
+              brand={ this.state.sideBarData[0].brand }
+            />
+            <Footer brand={ this.state.sideBarData[0].brand } />
+          </div>
+          <div className="feedingInstructionsClickedFalse"></div>
+        </div>
+      );
+    }
+    if (this.state.feedingInstructionsClicked) {
+      return (
+        <div>
+          <div className="descriptionTabClickedFalse"></div>
+          <div className="nutritionalInfoTabClickedFalse"></div>
+          <div className="feedingInstructionsClickedTrue">
+            <Header tabClicked={ this.tabClicked } />
+            <Description
+              description={ this.state.descriptionData }
+            />
+            <SideBar
+              itemDataTable={ this.state.sideBarData }
+              videoUrl={ this.state.descriptionData[0].videoUrl }
+              brand={ this.state.sideBarData[0].brand }
+            />
+            <Footer brand={ this.state.sideBarData[0].brand } />
+          </div>
+        </div>
+      );
+    }
+    // } else if (this.state.nutritionalInfoTabClicked) {
+    //   return (
+    //     <div className="nutritionalInfoTab">
+    //       <h1>Inside Nutritional Info Tab</h1>
+    //     </div>
+    //   )
+    // } else if (this.state.feedingInstructionsClicked) {
+    //   return (
+    //     <div className="feedingInstructionsTab">
+    //       <h1>Inside Feeding Instructions Tab</h1>
+    //     </div>
+    //   )
+    // }
   }
 
   render() {
